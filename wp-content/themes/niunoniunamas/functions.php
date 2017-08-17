0e7ed76aad20a1ca428553a42ca54f962cbb6e00
ponentesincausa <ponentesincausa@gmail.com>
2017-08-17 18:40:53 -0500

    removed wp address bar

--- wp-content/themes/niunoniunamas/functions.php
+++ wp-content/themes/niunoniunamas/functions.php
@@ -7,3 +7,8 @@ if ( has_post_thumbnail() ) {
 	the_post_thumbnail();
 }
 the_content();
+
+add_action('get_header', 'remove_admin_login_header');
+function remove_admin_login_header() {
+	remove_action('wp_head', '_admin_bar_bump_cb');
+}
